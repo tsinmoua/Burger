ec0045a0e47e9e32585d3befcf7beb1916f0b64f
Tsin Moua <tsinmoua@gmail.com>
2020-10-27 23:00:22 -0500

    Added connection and exported

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('\x1b[42m%s\x1b[0m', "\n Connected as ID: " + connection.threadId + "\n");
+});
+
+module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ INSERT INTO burgers
 (burger_name, devoured)
 VALUES
 ('BLT Burger', true),
-('Double Cheeseburger', true),
-('Avocado Burger', true);
+('Double Cheeseburger', false),
+('Avocado Burger', false);
