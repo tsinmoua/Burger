70711589524779a492ca8175ff9221e1d219e9bf
Tsin Moua <tsinmoua@gmail.com>
2020-10-28 20:05:31 -0500

    Added to orm.js and burger.js

--- config/orm.js
+++ config/orm.js
@@ -2,32 +2,38 @@ const connection = require("./connection.js");
 
 const orm = {
 
-    selectAll: function (tableInput) {
+    selectAll: function (tableInput, cb) {
         const queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function (err, result) {
             if (err) throw err;
             console.table(result);
-            return result
+            cb(result)
         });
     },
 
-    insertOne: function (tableInput, columnName, inputValue) {
-        const queryString = "INSERT INTO ?? (??) VALUES ??";
+    insertOne: function (tableInput, columnName, inputValue, cb) {
+        const queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [tableInput, columnName, inputValue], function (err, result) {
             if (err) throw err;
-            console.table(result);
-            return result
+            console.log(result);
+            cb(result)
         });
     },
 
-    updateOne: function (tableInput, columnName, inputValue, idColumnName, inputIdValue) {
-        const queryString = "UPDATE ?? SET ?? = ?? WHERE ?? = ??";
-        connection.query(queryString, [tableInput, columnName, inputValue, idColumnName, inputIdValue], function (err, result) {
+    updateOne: function (tableInput, columnName, inputValue, inputIdValue, cb) {
+        const queryString = "UPDATE ?? SET ?? = ? WHERE id = ?";
+        connection.query(queryString, [tableInput, columnName, inputValue, inputIdValue], function (err, result) {
             if (err) throw err;
             console.table(result);
-            return result
+            cb(result)
         });
-    };
+    }
 };
 
 module.exports = orm; 
+
+// orm.selectAll("burgers");
+// orm.insertOne("burgers", ["burger_name", "devoured"], ["test3", true]);
+// orm.selectAll("burgers");
+// orm.updateOne("burgers", "burger_name", "BLT + Cheeseburger", "1");
+// orm.selectAll("burgers");
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,27 @@
 const orm = require("../config/orm.js");
 
+const burger = {
 
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
 
+    insertOne: function (columnName, inputValue, cb) {
+        orm.insertOne("burgers", columnName, inputValue, function (res) {
+            cb(res);
+        });
+    },
 
+    updateOne: function (columnName, inputValue, inputIdValue, cb) {
+        orm.updateOne("burgers", columnName, inputValue, inputIdValue, function (res) {
+            cb(res);
+        });
+    },
 
+}
 
 module.export = burger;
+
+burger.selectAll();
