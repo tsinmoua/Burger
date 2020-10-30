4a65ec3acc35e7342039038b877fe4ac6f53b121
Tsin Moua <tsinmoua@gmail.com>
2020-10-29 21:33:26 -0500

    Changed port

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
     host: "mysql",
-    // port: 3306,
+    port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db", 
